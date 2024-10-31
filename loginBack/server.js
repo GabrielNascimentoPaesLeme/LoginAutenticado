9d682fc284ee6bb84a94a800592a70fdc8910ad2
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-10-31 11:41:58 -0300

    Correção caminho front

--- loginBack/server.js
+++ loginBack/server.js
@@ -15,7 +15,7 @@ const prisma = new PrismaClient()
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:5173', 'https://login-autenticado-robk.vercel.app'],
-  credentials: true,
+  credentials: false,
 }));
 
 async function connectToDatabase() {
